c9d5dd7f280e23437f8511618ebea10d447f0070
Michael Ogbuma <ogbumamichael@gmail.com>
2023-11-29 04:19:18 +0100

    - fix db connection

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://0.0.0.0:27017/todoListDb');
+mongoose.connect('mongodb://0.0.0.0:27017/todoListDb', { retryWrites: true, w: 'majority' });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
